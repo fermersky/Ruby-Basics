71595b7166f3f57efc309078575e814373b8367f
Daniel Skrypnik <forte201253@gmail.com>
2019-01-07 15:57:01 +0200

    Add files via upload

---
+++ basics.rb
@@ -0,0 +1,126 @@
+puts "Hello, World"
+
+# Variables and Data Types
+a = 5 # int
+b = 5.34 # float
+name = "Daniel" # string
+isMale = true # boolean
+a_symbol = :itsasymbol # symbol
+b_symbol = :itsasymbol
+
+print (name + a.to_s) # concatination
+print "#{name} #{a.to_s}"
+# object id's of intances of symbol with equal value have equal object.id, but strings always have different object id
+
+# conditional statement
+if a > b
+  puts "yeap"
+elsif a < b
+  # puts "nope"
+else
+  puts "equal"
+end
+
+def hey_ho?
+  true
+end
+puts "let’s go" if hey_ho? # if statement after the code
+
+# while loop
+num = 0
+while num < 10
+  num += 1
+  #puts num
+end
+
+# for loop
+for num in 1...10
+  # puts num
+end
+
+# each lopp
+[23, 1, 7, 62, 12].each do |num|
+  # print "#{num} "
+end
+
+# Arrays
+array = [1,2,3,4,5]
+names = [
+  "Daniel",
+  "Alina",
+  "Nastya"
+]
+names.push("Yarik");
+names << "Serega" # equal to names.<<("Serega")
+
+# Key-Value
+simple_hash = {
+  "name" => "Daniel",
+  "about" => "is a handsome fellow"
+}
+simple_hash["age"] = 18 # dynamicly add new key-value ^-^
+puts "#{simple_hash["name"]} #{simple_hash["about"]} #{simple_hash["age"]}"
+
+# each loop for hash table
+simple_hash.each do |key, value|
+  # puts "key - #{key}; value - #{value}"
+end
+
+# Classes
+class Vehicle
+  attr_reader :mark # only for read
+  attr_writer :model # only for write
+  attr_accessor :count # both
+
+  def initialize(mark, model, count) # constructor
+    @mark = mark
+    @model = model
+    @count = count
+  end
+end
+
+veh = Vehicle.new("Audi", "RS8", 4) # creat instance of class
+veh.model = "Bmw"
+# puts veh.mark
+
+module Skills
+  def speedUp
+    puts "Fastest man in the World"
+  end
+
+  # A module can have no instances.
+  # A module can have no subclasses.
+end
+
+class Person
+  include Skills # using module
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def name # getter
+    @name
+  end
+
+  def age # getter
+    @age
+  end
+
+  def isDaniel # class method
+    if @name == "Daniel"
+      true # return true ^-^
+    else
+      false
+    end
+  end
+
+  def to_s
+    "#{name} #{age}"
+  end
+end
+
+per = Person.new("Daniel", 18)
+per.speedUp
+puts per.to_s
